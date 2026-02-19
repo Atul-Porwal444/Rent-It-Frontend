b921814d1f29833467e201c420ef8890ece8eb84
Atul <aatulporwal999@gmail.com>
2026-02-19 19:51:49 +0530

    Added the filed like state and pincode in the listing form.

--- src/app/_post-ad/post-ad/post-ad.component.ts
+++ src/app/_post-ad/post-ad/post-ad.component.ts
@@ -20,6 +20,16 @@ export class PostAdComponent {
   selectedImages: File[] = [];
   imagePreviews: string[] = [];
 
+  indianStates: string[] = [
+    "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", 
+    "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand", "Karnataka", "Kerala", 
+    "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", 
+    "Odisha", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura", 
+    "Uttar Pradesh", "Uttarakhand", "West Bengal", "Andaman and Nicobar Islands", 
+    "Chandigarh", "Dadra and Nagar Haveli", "Daman and Diu", "Delhi", "Lakshadweep", 
+    "Puducherry"
+  ].sort();
+
   indianCities: string[] = [
     'SGM',
     'STR',
@@ -5191,6 +5201,8 @@ export class PostAdComponent {
   formData: any = {
     location: '',
     city: '',
+    state: '',
+    pincode: null,
     description: '',
     rentAmount: null,
     bhkType: '1BHK',
