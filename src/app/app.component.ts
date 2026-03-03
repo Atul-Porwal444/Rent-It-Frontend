26544b13eac53b4ed738d8071ebbb0501698bce1
Atul <aatulporwal999@gmail.com>
2026-03-03 21:55:57 +0530

    Added the footer component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HttpClientModule, FormsModule, ],
+  imports: [RouterOutlet, NavbarComponent, HttpClientModule, FormsModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
